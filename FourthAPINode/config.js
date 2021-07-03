a29900953600143378e47e7fbb0dbe76641998f3
Tamires Cavalcante <tmires.cavalcante@gmail.com>
2021-07-03 16:11:16 -0300

    Adding a fourth api node

---
+++ FourthAPINode/config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MONGO_URI: 'mongodb://hello:uCoiQfSeaz4AXe4L@cluster0-shard-00-00.mtgy6.mongodb.net:27017,cluster0-shard-00-01.mtgy6.mongodb.net:27017,cluster0-shard-00-02.mtgy6.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-ufssy9-shard-0&authSource=admin&retryWrites=true&w=majority'
+}
